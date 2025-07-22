75e840aa3e344f0d15d9d36d83b8bf99ab0f526e
Angel Sanchez <percycsa@hotmail.com>
2025-07-22 01:12:11 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ad-banner.tsx
+++ src/components/ad-banner.tsx
@@ -1,19 +1,7 @@
 
 "use client";
 
-import { useEffect } from 'react';
-
 export function AdBanner() {
-
-  useEffect(() => {
-    try {
-      // @ts-ignore
-      (adsbygoogle = window.adsbygoogle || []).push({});
-    } catch (e) {
-      console.error("AdSense error:", e);
-    }
-  }, []);
-
   return (
     <div className="my-6 flex justify-center items-center w-full h-[90px] bg-muted/30 rounded-lg overflow-hidden">
       <ins 
